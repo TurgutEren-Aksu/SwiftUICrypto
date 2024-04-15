4d93b479758fde0d9439794acc1ee744c74dc097
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-04-15 03:16:16 +0300

    Initial Commit

---
+++ SwiftUICrypto/SwiftUICryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICryptoApp.swift
+//  SwiftUICrypto
+//
+//  Created by Turgut Eren Aksu on 15.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
