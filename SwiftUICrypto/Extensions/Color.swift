e6cbc151ba7a1486b2cb713f5c98c18ae4594456
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-04-17 11:40:42 +0300

    Color

---
+++ SwiftUICrypto/Extensions/Color.swift
@@ -0,0 +1,23 @@
+//
+//  Color.swift
+//  SwiftUICrypto
+//
+//  Created by Turgut Eren Aksu on 17.04.2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct ColorTheme{
+	let accent = Color("AccentColor")
+	let background = Color("BackgroundColor")
+	let green = Color("GreenColor")
+	let red = Color("RedColor")
+	let secondaryText = Color("SecondaryTextColor")
+}
+
+extension Color {
+	
+	static let theme = ColorTheme()
+	
+}
