4b5104952aeac469a54270932d076bb7dabf67b5
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-04-20 01:05:26 +0300

    CircleButtonView

---
+++ SwiftUICrypto/Core/Components/CircleButton/CircleButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  CircleButtonView.swift
+//  SwiftUICrypto
+//
+//  Created by Turgut Eren Aksu on 19.04.2024.
+//
+
+import SwiftUI
+
+struct CircleButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleButtonView()
+		.padding()
+		.previewLayout(.sizeThatFits)
+}
