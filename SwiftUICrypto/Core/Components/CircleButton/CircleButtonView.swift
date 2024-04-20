031bf74bcf22176f8c5feb81c02a4e66e2e7d2a6
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-04-21 02:01:14 +0300

    Image CircleButtonView

--- SwiftUICrypto/Core/Components/CircleButton/CircleButtonView.swift
+++ SwiftUICrypto/Core/Components/CircleButton/CircleButtonView.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct CircleButtonView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		Image(systemName: "heart.fill")
+			.font(.headline)
+			.foregroundColor(Color.theme.accent)
+			.frame(width: 50, height: 50)
+			.background(
+			Circle()
+				.foregroundColor(Color.theme.background)
+			)
+			.shadow(
+				color: Color.theme.accent.opacity(0.25),
+				radius: 10,x: 0,y: 0)
+			.padding()
     }
 }
 
