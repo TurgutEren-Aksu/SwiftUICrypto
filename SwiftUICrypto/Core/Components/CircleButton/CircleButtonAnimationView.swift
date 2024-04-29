a972971af57245621ffe5cd86e3cd11ef2e4aaa4
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-04-29 19:14:22 +0300

    CircleButtonAnimationView

---
+++ SwiftUICrypto/Core/Components/CircleButton/CircleButtonAnimationView.swift
@@ -0,0 +1,27 @@
+//
+//  CircleButtonAnimationView.swift
+//  SwiftUICrypto
+//
+//  Created by Turgut Eren Aksu on 29.04.2024.
+//
+
+import SwiftUI
+
+struct CircleButtonAnimationView: View {
+	
+	@State private var animate: Bool = false
+	
+    var body: some View {
+		Circle()
+			.stroke(lineWidth: 5.0)
+			.scale(animate ? 1.0 : 0.0)
+			.onAppear{
+				animate.toggle()
+			}
+		
+    }
+}
+
+#Preview {
+    CircleButtonAnimationView()
+}
