ec48ea5cc1a07a4106d7a6be8333fd80cf9e6ebc
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-05-01 17:01:02 +0300

    opacity

--- SwiftUICrypto/Core/Components/CircleButton/CircleButtonAnimationView.swift
+++ SwiftUICrypto/Core/Components/CircleButton/CircleButtonAnimationView.swift
@@ -15,6 +15,7 @@ struct CircleButtonAnimationView: View {
 		Circle()
 			.stroke(lineWidth: 5.0)
 			.scale(animate ? 1.0 : 0.0)
+			.opacity(animate ? 0.0 : 1.0)
 			.animation(animate ? Animation.easeOut(duration: 1.0) : .none)
 			.onAppear{
 				animate.toggle()
