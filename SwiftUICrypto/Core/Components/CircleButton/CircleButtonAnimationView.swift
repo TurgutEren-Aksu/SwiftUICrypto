69b5fd0d5dc4d196316be741f6141b66cb73ec93
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-05-03 15:46:01 +0300

    Binding

--- SwiftUICrypto/Core/Components/CircleButton/CircleButtonAnimationView.swift
+++ SwiftUICrypto/Core/Components/CircleButton/CircleButtonAnimationView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct CircleButtonAnimationView: View {
 	
-	@State private var animate: Bool = false
+	@Binding var animate: Bool
 	
     var body: some View {
 		Circle()
@@ -17,15 +17,12 @@ struct CircleButtonAnimationView: View {
 			.scale(animate ? 1.0 : 0.0)
 			.opacity(animate ? 0.0 : 1.0)
 			.animation(animate ? Animation.easeOut(duration: 1.0) : .none)
-			.onAppear{
-				animate.toggle()
-			}
 		
     }
 }
 
 #Preview {
-    CircleButtonAnimationView()
+	CircleButtonAnimationView(animate: .constant(false))
 		.foregroundStyle(.red)
 		.frame(width: 100,height: 100)
 }
