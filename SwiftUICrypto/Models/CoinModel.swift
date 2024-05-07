112a2c3928262e87498f032271073d7cb2c5fee4
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-05-07 07:52:12 +0300

    CoinGecko Request

--- SwiftUICrypto/Models/CoinModel.swift
+++ SwiftUICrypto/Models/CoinModel.swift
@@ -7,6 +7,22 @@
 
 import Foundation
 
-class CoinModel {
-	
+struct CoinModel {
+		let id, symbol, name: String
+		let image: String
+		let currentPrice, marketCap, marketCapRank, fullyDilutedValuation: Int
+		let totalVolume, high24H, low24H: Int
+		let priceChange24H, priceChangePercentage24H, marketCapChange24H, marketCapChangePercentage24H: Double
+		let circulatingSupply, totalSupply, maxSupply, ath: Int
+		let athChangePercentage: Double
+		let athDate: String
+		let atl, atlChangePercentage: Double
+		let atlDate: String
+		let roi: NSNull
+		let lastUpdated: String
+		let sparklineIn7D: SparklineIn7D
+		let priceChangePercentage24HInCurrency: Double
+}
+struct SparklineIn7D {
+	let price: [Double]
 }
