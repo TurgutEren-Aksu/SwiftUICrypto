7f52c9d63af259b18b694ee7dd6405438a3d7bb5
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-05-09 07:30:59 +0300

    enum CodingKeys

--- SwiftUICrypto/Models/CoinModel.swift
+++ SwiftUICrypto/Models/CoinModel.swift
@@ -25,6 +25,35 @@ struct CoinModel: Identifiable,Codable {
 		let sparklineIn7D: SparklineIn7D?
 		let priceChangePercentage24HInCurrency: Double?
 		let currentHoldings: Double?
+	
+	enum CodingKeys: String, CodingKey{
+		case id,symbol,name,image
+		case currentPrice = "curren_price"
+		case marketCap = "market_cap"
+		case marketCapRank = "market_cap_rank"
+		case fullyDilutedValuation = "fully_diluted_valuation"
+		case totalVolume = "total_volume"
+		case high24 = "high_24h"
+		case low24 = "low_24"
+		case priceChance24 = "price_change_24h"
+		case priceChangePercentage24 = "price_change_percentage_24h"
+		case marketCapChange24 = "market_cap_change_24h"
+		case marketCapChangePercentage24 = "market_cap_change_percentage_24h"
+		case circulatingSupply = "circulating_supply"
+		case totalSupply = "total_supply"
+		case maxSupply = "max_supply"
+		case ath
+		case athChangePercentage = "ath_change_percentage"
+		case athDate = "ath_date"
+		case atl
+		case atlChangePercentage = "atl_change_percentage"
+		case atlDate = "atl_date"
+		case lastUpdated = "last_updated"
+		case sparklineIn7d = "sparkline_in_7d"
+		case priceChangePercentage24hInCurrency = "price_change_percentage_24h_in_currency"
+		case currentHoldings
+	}
+	
 }
 struct SparklineIn7D:Codable {
 	let price: [Double]?
