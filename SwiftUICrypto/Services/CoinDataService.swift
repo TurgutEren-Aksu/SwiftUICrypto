cb448da4f3fe44ecd7bf5590fc168934f283ffee
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-06-06 02:52:27 +0300

    CoinDataService

---
+++ SwiftUICrypto/Services/CoinDataService.swift
@@ -0,0 +1,12 @@
+//
+//  CoinDataService.swift
+//  SwiftUICrypto
+//
+//  Created by Turgut Eren Aksu on 6.06.2024.
+//
+
+import Foundation
+
+class CoinDataService {
+	@Published var allCoins: [CoinModel] = []
+}
