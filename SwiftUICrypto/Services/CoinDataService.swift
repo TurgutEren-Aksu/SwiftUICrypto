1671d6bfe8d44ac465e515f6e426318b891a7b5c
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-06-09 02:58:21 +0300

    Response Status

--- SwiftUICrypto/Services/CoinDataService.swift
+++ SwiftUICrypto/Services/CoinDataService.swift
@@ -18,6 +18,12 @@ class CoinDataService {
 		guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=250&page=1&sparkline=true&price_change_percentage=24h#") else{return}
 		URLSession.shared.dataTaskPublisher(for: url)
 			.subscribe(on: DispatchQueue.global(qos: .default))
+			.tryMap { (output) -> Data in
+				guard let response = output.response as? HTTPURLResponse,response.statusCode >= 200 && response.statusCode < 300 else {
+					throw URLError(.badServerResponse)
+				}
+				return output.data
+			}
 	}
 	
 }
