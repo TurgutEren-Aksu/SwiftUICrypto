94c63d5e33f0d330027000640c3ae68cff6199ba
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-06-08 21:49:25 +0300

    URLSession

--- SwiftUICrypto/Services/CoinDataService.swift
+++ SwiftUICrypto/Services/CoinDataService.swift
@@ -16,6 +16,8 @@ class CoinDataService {
 	
 	private func getCoins(){
 		guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=250&page=1&sparkline=true&price_change_percentage=24h#") else{return}
+		URLSession.shared.dataTaskPublisher(for: url)
+			.subscribe(on: DispatchQueue.global(qos: .default))
 	}
 	
 }
