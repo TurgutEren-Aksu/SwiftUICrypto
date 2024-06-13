b9faa4c7b179b09aac193c91d9cfef3c72bb07c8
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-06-13 03:14:52 +0300

    Cancel subscription

--- SwiftUICrypto/Services/CoinDataService.swift
+++ SwiftUICrypto/Services/CoinDataService.swift
@@ -10,7 +10,7 @@ import Combine
 
 class CoinDataService {
 	@Published var allCoins: [CoinModel] = []
-	var cancellables = Set<AnyCancellable>()
+	var coinSubscription: AnyCancellable?
 	
 	init() {
 		getCoins()
@@ -18,7 +18,7 @@ class CoinDataService {
 	
 	private func getCoins(){
 		guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=250&page=1&sparkline=true&price_change_percentage=24h#") else{return}
-		URLSession.shared.dataTaskPublisher(for: url)
+		coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
 			.subscribe(on: DispatchQueue.global(qos: .default))
 			.tryMap { (output) -> Data in
 				guard let response = output.response as? HTTPURLResponse,response.statusCode >= 200 && response.statusCode < 300 else {
@@ -39,9 +39,9 @@ class CoinDataService {
 				
 			} receiveValue: { [weak self ](returnCoins) in
 				self?.allCoins = returnCoins
+				self?.coinSubscription?.cancel()
 				
 			}
-			.store(in: &cancellables)
 	}
 	
 }
