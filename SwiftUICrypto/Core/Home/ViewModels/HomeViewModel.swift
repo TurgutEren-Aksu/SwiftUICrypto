2a63cdc8c3539ec1e62361ebf5fc0677dd6352b3
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-06-14 15:42:22 +0300

    Coin Data Services

--- SwiftUICrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftUICrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -10,6 +10,7 @@ import Foundation
 class HomeViewModel: ObservableObject{
 	@Published var allCoins: [CoinModel] = []
 	@Published var portfolioCoins: [CoinModel] = []
+	private let dataService = CoinDataService() 
 	init() {
 		DispatchQueue.main.asyncAfter(deadline: .now()+2.0){
 			self.allCoins.append(DeveloperProvider.instance.coin)
