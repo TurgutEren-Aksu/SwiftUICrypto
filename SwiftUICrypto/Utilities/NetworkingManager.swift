3dd8b84c976a93a37cebd6152442c185f5714390
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-06-17 17:28:38 +0300

    Networking layer

--- SwiftUICrypto/Utilities/NetworkingManager.swift
+++ SwiftUICrypto/Utilities/NetworkingManager.swift
@@ -6,12 +6,13 @@
 //
 
 import Foundation
+import Combine
 
 
 class NetworkingManager {
-	static func download(url: URL) {
-		guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=250&page=1&sparkline=true&price_change_percentage=24h#") else{return}
-		URLSession.shared.dataTaskPublisher(for: url)
+	static func download(url: URL) -> Publishers.ReceiveOn<Publishers.TryMap<Publishers.SubscribeOn<URLSession.DataTaskPublisher, DispatchQueue>, Data>, DispatchQueue>{
+		
+	return URLSession.shared.dataTaskPublisher(for: url)
 			.subscribe(on: DispatchQueue.global(qos: .default))
 			.tryMap { (output) -> Data in
 				guard let response = output.response as? HTTPURLResponse,response.statusCode >= 200 && response.statusCode < 300 else {
