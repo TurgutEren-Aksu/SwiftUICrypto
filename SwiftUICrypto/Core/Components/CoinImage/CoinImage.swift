f1e38d33237f4849fa5b9149d5bd4bc516044c14
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-06-26 20:38:43 +0300

    Coin Image

---
+++ SwiftUICrypto/Core/Components/CoinImage/CoinImage.swift
@@ -0,0 +1,30 @@
+//
+//  CoinImage.swift
+//  SwiftUICrypto
+//
+//  Created by Turgut Eren Aksu on 26.06.2024.
+//
+
+import SwiftUI
+
+class CoinImageViewModel: ObservableObject {
+	@Published var image: UIImage? = nil
+	@Published var isLoading: Bool = false
+	
+	init(){
+		
+	}
+	
+	
+}
+
+
+struct CoinImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoinImage()
+}
