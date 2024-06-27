713828f6cef3b5cfb9e2b107aeed7bc4925ecde7
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-06-27 20:56:02 +0300

    getImage

--- SwiftUICrypto/Core/Components/CoinImage/CoinImage.swift
+++ SwiftUICrypto/Core/Components/CoinImage/CoinImage.swift
@@ -14,12 +14,17 @@ class CoinImageViewModel: ObservableObject {
 	init(){
 		
 	}
-	
+	private func getImage(){
+		
+	}
 	
 }
 
 
 struct CoinImage: View {
+	
+	@StateObject var vm: CoinImageViewModel = CoinImageViewModel()
+	
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
