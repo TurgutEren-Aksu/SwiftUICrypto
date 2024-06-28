b31d08039d77cb7c0bd65b788c6891e91a9ab7e3
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-06-28 16:40:20 +0300

    if let image

--- SwiftUICrypto/Core/Components/CoinImage/CoinImage.swift
+++ SwiftUICrypto/Core/Components/CoinImage/CoinImage.swift
@@ -26,7 +26,19 @@ struct CoinImage: View {
 	@StateObject var vm: CoinImageViewModel = CoinImageViewModel()
 	
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		ZStack{
+			if let image = vm.image{
+				Image(uiImage: image)
+					.resizable()
+					.scaledToFit()
+			} else if vm.isLoading{
+				ProgressView()
+			}else{
+				Image(systemName: "quesitonmark")
+					.foregroundStyle(Color.theme.secondaryText)
+				
+			}
+		}
     }
 }
 
