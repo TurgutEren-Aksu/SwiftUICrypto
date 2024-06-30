c2a9b3bb3a02df7eac897e5b83eb801be23f6f67
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-06-30 14:43:14 +0300

    CoinImageService

---
+++ SwiftUICrypto/Services/CoinImageService.swift
@@ -0,0 +1,20 @@
+//
+//  CoinImageService.swift
+//  SwiftUICrypto
+//
+//  Created by Turgut Eren Aksu on 30.06.2024.
+//
+
+import Foundation
+import SwiftUI
+
+class CoinImageService{
+	
+	@Published var image: UIImage? = nil
+	init() {
+		getCoinImage()
+	}
+	func getCoinImage() {
+		
+	}
+}
