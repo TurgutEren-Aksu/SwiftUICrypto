630c0bb796b700cf74cbb1ef3061c36cd1044bd1
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-07-01 08:02:48 +0300

    getCoinImage

--- SwiftUICrypto/Services/CoinImageService.swift
+++ SwiftUICrypto/Services/CoinImageService.swift
@@ -7,14 +7,24 @@
 
 import Foundation
 import SwiftUI
+import Combine
 
 class CoinImageService{
+	var imageSubscription: AnyCancellable?
 	
 	@Published var image: UIImage? = nil
-	init() {
-		getCoinImage()
+	init(urlString: String) {
+		getCoinImage(urlString: <#T##String#>)
 	}
-	func getCoinImage() {
-		
+	func 	(urlString: String) {
+		guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=250&page=1&sparkline=true&price_change_percentage=24h#") else{return}
+		imageSubscription = NetworkingManager.download(url: url)
+			.tryMap({ (data) -> UIImage? in
+				return UIImage(data: data)
+			})
+			.sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self ] (returnedImage) in
+				self?.image = returnedImage
+				self?.imageSubscription?.cancel()
+			})
 	}
 }
