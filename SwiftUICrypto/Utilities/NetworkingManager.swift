4fd1e90dfbfac03786966c1c9ef749e3f35dc982
TurgutEren-Aksu <turguterenaksu@outlook.com>
2024-07-25 06:35:15 +0300

    NetworkingManager download funciton

---
+++ SwiftUICrypto/Utilities/NetworkingManager.swift
@@ -0,0 +1,26 @@
+//
+//  NetworkingManager.swift
+//  SwiftUICrypto
+//
+//  Created by Turgut Eren Aksu on 25.07.2024.
+//
+
+import Foundation
+import Combine
+
+class NetworkingManager{
+    
+    static func download(url:URL) -> AnyPublisher<Data,Error>{
+       return URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap { (output) -> Data in
+                guard let response = output.response as? HTTPURLResponse,response.statusCode >= 200 && response.statusCode < 300 else {
+                    throw URLError(.badServerResponse)
+                }
+                return output.data
+            }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+    
+}
