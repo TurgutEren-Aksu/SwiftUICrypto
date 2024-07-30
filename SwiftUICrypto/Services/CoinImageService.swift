5906efeb4d1669cd45d228dc8cbce1b93da6f529
TurgutEren-Aksu <turguterenaksu@outlook.com>
2024-07-31 00:08:53 +0300

    getCoinImage

--- SwiftUICrypto/Services/CoinImageService.swift
+++ SwiftUICrypto/Services/CoinImageService.swift
@@ -6,7 +6,29 @@
 //
 
 import Foundation
+import SwiftUI
+import Combine
 
 class CoinImageService{
+    
+    @Published var image: UIImage? = nil
+    
+    var imageSubscription: AnyCancellable?
+    
+    init(){
+//        getCoinImage()
+    }
+    
+    func getCoinImage(urlString: String){
+        guard let url = URL(string: urlString ) else{return}
+        imageSubscription = NetworkingManager.download(url: url)
+        //trymap
+            .decode(type: [CoinModel].self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnCoins) in
+                self?.allCoins = returnCoins
+                self?.coinSubscription?.cancel()
+            })
+
+    }
     
 }
