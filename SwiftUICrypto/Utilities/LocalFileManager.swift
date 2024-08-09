58593dd3a7ded04023226060f0033769096bd370
TurgutEren-Aksu <turguterenaksu@outlook.com>
2024-08-09 20:38:52 +0300

    create LocalFileManager

---
+++ SwiftUICrypto/Utilities/LocalFileManager.swift
@@ -0,0 +1,14 @@
+//
+//  LocalFileManager.swift
+//  SwiftUICrypto
+//
+//  Created by Turgut Eren Aksu on 9.08.2024.
+//
+
+import Foundation
+
+class LocalFileManager{
+    
+    static let instance = LocalFileManager()
+    
+}
