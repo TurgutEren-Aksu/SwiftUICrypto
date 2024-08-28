36e24741e22a4c2781208b7cab699552e864dfc3
TurgutEren-Aksu <turguterenaksu@outlook.com>
2024-08-28 19:34:51 +0300

    StatisticView

---
+++ SwiftUICrypto/Core/Components/StatisticView.swift
@@ -0,0 +1,18 @@
+//
+//  StatisticsView.swift
+//  SwiftUICrypto
+//
+//  Created by Turgut Eren Aksu on 28.08.2024.
+//
+
+import SwiftUI
+
+struct StatisticView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatisticView()
+}
