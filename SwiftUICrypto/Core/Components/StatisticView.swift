c62dc86406aeb9e9bae8096434754f4fa56ee9f5
TurgutEren-Aksu <turguterenaksu@outlook.com>
2024-08-29 05:14:17 +0300

    StatisticView

--- SwiftUICrypto/Core/Components/StatisticView.swift
+++ SwiftUICrypto/Core/Components/StatisticView.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct StatisticView: View {
+    let stat: StatisticModel 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 4){
+            Text(stat.title)
+                .font(.caption)
+                .foregroundStyle(Color.theme.secondaryText)
+            Text(stat.value)
+                .font(.headline)
+                .foregroundStyle(Color.theme.accent)
+            HStack(spacing: 4){
+                
+                Image(systemName: "triangle.fill")
+                    .font(.caption2)
+                    .rotationEffect(
+                        Angle(degrees: (stat.percentageChange ?? 0) >= 0 ? 0 : 180)
+                    
+                    )
+                
+                Text(stat.percentageChange?.asPercentString() ?? "")
+                    .font(.caption)
+                    .bold()
+            }
+            .foregroundStyle(Color((stat.percentageChange ?? 0) >= 0 ? Color.theme.green : Color.theme.red))
+            .opacity(stat.percentageChange == nil ? 0.0 : 1.0)
+            
+            
+        }
+        
     }
 }
 
 #Preview {
-    StatisticView()
+    StatisticView(stat: DeveloperProvider.instance.state1)
 }
