92c992771f6449e9d3a9ea59cfc03dea79c84f9e
TurgutEren-Aksu <turguterenaksu@outlook.com>
2024-09-01 16:35:40 +0300

    HomeStatsView showPortfolio

--- SwiftUICrypto/Core/Home/Views/HomeStatsView.swift
+++ SwiftUICrypto/Core/Home/Views/HomeStatsView.swift
@@ -9,24 +9,23 @@ import SwiftUI
 
 struct HomeStatsView: View {
     
-    let statistics: [StatisticModel] = [
-    StatisticModel(title: "Title", value: "Value", percentageChange: 1),
-    StatisticModel(title: "Title", value: "Value"),
-    StatisticModel(title: "Title", value: "Value"),
-    StatisticModel(title: "Title", value: "Value", percentageChange: -7),
-    ]
+    @EnvironmentObject private var vm : HomeViewModel
     
+    @Binding var showPortfolio: Bool
     
     var body: some View {
         HStack{
-            ForEach(statistics){ stat in
+            ForEach(vm.statistics){ stat in
                 StatisticView(stat: stat)
+                    .frame(width: UIScreen.main.bounds.width / 3)
                 
             }
         }
+        .frame(width:  UIScreen.main.bounds.width, alignment: showPortfolio ? .trailing : .leading)
     }
 }
 
 #Preview {
-    HomeStatsView()
+    HomeStatsView(showPortfolio: .constant(false))
+        .environmentObject(DeveloperProvider.instance.homeVM)
 }
