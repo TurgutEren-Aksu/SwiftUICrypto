3b1903a06b436ee1ce80a25d4657bef568db0390
TurgutEren-Aksu <turguterenaksu@outlook.com>
2024-09-09 13:27:17 +0300

    Add mapping of market data to statistic models for UI display.

--- SwiftUICrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftUICrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -38,6 +38,26 @@ class HomeViewModel: ObservableObject{
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+        marketDataService.$marketData
+            .map{(marketDataModel) -> [StatisticModel] in
+                var stats: [StatisticModel] = []
+                guard let data = marketDataModel else {
+                    return stats
+                }
+                let marketCap = StatisticModel(title: "MarketCap", value: data.marketCap, percentageChange: data.marketCapChangePercentage24HUsd)
+                stats.append(marketCap)
+                let volume = StatisticModel(title: "24H Volume", value: data.volume)
+                let btcDominance = StatisticModel(title: "BTC Dominance", value: data.btcDominance)
+                let portfolio = StatisticModel(title: "Portfolia Value", value: "$0.00", percentageChange: 0)
+                stats.append(volume)
+                stats.append(contentsOf: [
+                marketCap,
+                volume,
+                btcDominance,
+                portfolio
+                ])
+                return stats
+            }
     }
     private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel]{
         guard !text.isEmpty else {
