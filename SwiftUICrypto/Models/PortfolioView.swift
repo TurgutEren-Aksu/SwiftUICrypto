87b0cca4ecf50803edf88ee27a60b0a493cca7f0
TurgutEren-Aksu <turguterenaksu@outlook.com>
2024-09-14 17:16:07 +0300

    PortfolioView

---
+++ SwiftUICrypto/Models/PortfolioView.swift
@@ -0,0 +1,18 @@
+//
+//  PortfolioView.swift
+//  SwiftUICrypto
+//
+//  Created by Turgut Eren Aksu on 14.09.2024.
+//
+
+import SwiftUI
+
+struct PortfolioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PortfolioView()
+}
