05c16dab46cef5bb1555574987189d82211904a1
TurgutEren-Aksu <turguterenaksu@outlook.com>
2024-09-18 03:52:00 +0300

    XMarkButton

---
+++ SwiftUICrypto/Core/Components/CircleButton/XMarkButton.swift
@@ -0,0 +1,18 @@
+//
+//  XMarkButton.swift
+//  SwiftUICrypto
+//
+//  Created by Turgut Eren Aksu on 18.09.2024.
+//
+
+import SwiftUI
+
+struct XMarkButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    XMarkButton()
+}
