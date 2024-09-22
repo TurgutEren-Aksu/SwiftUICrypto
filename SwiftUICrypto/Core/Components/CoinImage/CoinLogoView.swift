babff781672ddd990f2a88c381ca7b57c9239362
TurgutEren-Aksu <turguterenaksu@outlook.com>
2024-09-22 11:27:11 +0300

    create CoinLogoView

---
+++ SwiftUICrypto/Core/Components/CoinImage/CoinLogoView.swift
@@ -0,0 +1,19 @@
+//
+//  CoinLogoView.swift
+//  SwiftUICrypto
+//
+//  Created by Turgut Eren Aksu on 22.09.2024.
+//
+
+import SwiftUI
+
+struct CoinLogoView: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoinLogoView()
+}
