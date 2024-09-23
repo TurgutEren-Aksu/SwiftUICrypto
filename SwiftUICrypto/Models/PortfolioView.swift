9fbcf1f520a795a78b7ab0103622ecb96e10057c
TurgutEren-Aksu <turguterenaksu@outlook.com>
2024-09-24 01:12:21 +0300

    coinLogoList selectedCoin

--- SwiftUICrypto/Models/PortfolioView.swift
+++ SwiftUICrypto/Models/PortfolioView.swift
@@ -9,27 +9,14 @@ import SwiftUI
 
 struct PortfolioView: View {
     @EnvironmentObject private var vm: HomeViewModel
+    @State private var selectedCoin: CoinModel? = nil
+    
     var body: some View {
         NavigationView {
             ScrollView{
                 VStack(alignment: .leading,spacing: 0){
                     SearchBarView(searchText: $vm.searchText)
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        LazyHStack(spacing: 10){
-                            ForEach(vm.allCoins) { coin in
-                                CoinLogoView(coin: coin)
-                                    .frame(width: 75)
-                                    .padding(4)
-                                    .background(
-                                        RoundedRectangle(cornerRadius: 10)
-                                            .stroke(Color.theme.green,lineWidth: 1)
-                                        
-                                    )
-                            }
-                        }
-                        .padding(.vertical,4)
-                        .padding(.leading)
-                    }
+                    coinLogoList
                 }
             }
             .navigationTitle("Edit Portfolio")
@@ -45,4 +32,29 @@ struct PortfolioView: View {
 #Preview {
     PortfolioView()
         .environmentObject(DeveloperProvider.instance.homeVM)
+}
+extension PortfolioView{
+    private var coinLogoList: some View{
+        ScrollView(.horizontal, showsIndicators: false, content: {
+            LazyHStack(spacing: 10){
+                ForEach(vm.allCoins) { coin in
+                    CoinLogoView(coin: coin)
+                        .frame(width: 75)
+                        .padding(4)
+                        .onTapGesture {
+                            withAnimation(.easeIn) {
+                                selectedCoin = coin
+                            }
+                        }
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(selectedCoin?.id == coin.id ? Color.theme.green : Color.clear,lineWidth: 1)
+                            
+                        )
+                }
+            }
+            .padding(.vertical,4)
+            .padding(.leading)
+        })
+    }
 }
