873de96b825dfc752767975cb7728d76012de9b4
TurgutEren-Aksu <turguterenaksu@outlook.com>
2024-09-25 13:04:10 +0300

    Add UI for displaying selected coin's current price and portfolio amount input

--- SwiftUICrypto/Models/PortfolioView.swift
+++ SwiftUICrypto/Models/PortfolioView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct PortfolioView: View {
     @EnvironmentObject private var vm: HomeViewModel
     @State private var selectedCoin: CoinModel? = nil
+    @State private var quantityText: String = ""
     
     var body: some View {
         NavigationView {
@@ -17,6 +18,22 @@ struct PortfolioView: View {
                 VStack(alignment: .leading,spacing: 0){
                     SearchBarView(searchText: $vm.searchText)
                     coinLogoList
+                    if selectedCoin != nil{
+                        VStack(spacing: 20){
+                            HStack{
+                                Text("Current price of \(selectedCoin?.symbol.uppercased() ?? ""):")
+                                Spacer()
+                                Text(selectedCoin?.currentPrice.asCurrencyWith2Decimals() ?? "")
+                            }
+                            Divider()
+                            HStack{
+                                Text("Amount in your portfolio:")
+                                Spacer()
+                                TextField("Ex: 1.4",text: $quantityText)
+                            }
+                            
+                        }
+                    }
                 }
             }
             .navigationTitle("Edit Portfolio")
