b3499bb4a5e39b442d206b9d0d315c64946b7d05
TurgutEren-Aksu <turguterenaksu@outlook.com>
2024-09-28 14:02:40 +0300

    portfolioInputSection

--- SwiftUICrypto/Models/PortfolioView.swift
+++ SwiftUICrypto/Models/PortfolioView.swift
@@ -19,31 +19,7 @@ struct PortfolioView: View {
                     SearchBarView(searchText: $vm.searchText)
                     coinLogoList
                     if selectedCoin != nil{
-                        VStack(spacing: 20){
-                            HStack{
-                                Text("Current price of \(selectedCoin?.symbol.uppercased() ?? ""):")
-                                Spacer()
-                                Text(selectedCoin?.currentPrice.asCurrencyWith2Decimals() ?? "")
-                            }
-                            Divider()
-                            HStack{
-                                Text("Amount in your portfolio:")
-                                Spacer()
-                                TextField("Ex: 1.4",text: $quantityText)
-                                    .multilineTextAlignment(.trailing)
-                                    .keyboardType(.decimalPad)
-                            }
-                            Divider()
-                            HStack{
-                                Text("Current Value:")
-                                Spacer()
-                                Text(getCurrentValue().asCurrencyWith2Decimals())
-                            }
-                            
-                        }
-                        .animation(.none)
-                        .padding()
-                        .font(.headline)
+                        portfolioInputSection
                     }
                 }
             }
@@ -81,7 +57,7 @@ extension PortfolioView{
                         )
                 }
             }
-            .padding(.vertical,4)
+            .frame(height: 120)
             .padding(.leading)
         })
     }
@@ -90,5 +66,32 @@ extension PortfolioView{
             return quantity * (selectedCoin?.currentPrice ?? 0)
         }
         return 0
+    }
+    private var portfolioInputSection: some View{
+        VStack(spacing: 20){
+            HStack{
+                Text("Current price of \(selectedCoin?.symbol.uppercased() ?? ""):")
+                Spacer()
+                Text(selectedCoin?.currentPrice.asCurrencyWith2Decimals() ?? "")
+            }
+            Divider()
+            HStack{
+                Text("Amount in your portfolio:")
+                Spacer()
+                TextField("Ex: 1.4",text: $quantityText)
+                    .multilineTextAlignment(.trailing)
+                    .keyboardType(.decimalPad)
+            }
+            Divider()
+            HStack{
+                Text("Current Value:")
+                Spacer()
+                Text(getCurrentValue().asCurrencyWith2Decimals())
+            }
+            
+        }
+        .animation(.none)
+        .padding()
+        .font(.headline)
     }
 }
