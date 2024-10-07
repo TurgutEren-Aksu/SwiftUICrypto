b9e0890e199bb1fb32bb53dfd4710f4a625e5e53
TurgutEren-Aksu <turguterenaksu@outlook.com>
2024-10-08 01:38:07 +0300

    Set up Core Data persistent container for portfolio management

--- SwiftUICrypto/Services/PortfolioDataService.swift
+++ SwiftUICrypto/Services/PortfolioDataService.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import CoreData
+
+class PortfolioDataService{
+    private let container: NSPersistentContainer
+    private let containerName: String = "PortfolioContainer"
+    init(){
+        container = NSPersistentContainer(name: containerName)
+        container.loadPersistentStores { (_, error) in
+            if let error = error{
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
