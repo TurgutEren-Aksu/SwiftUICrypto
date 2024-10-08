192256097a2ab9257968a77311cb8e256f9b0a99
TurgutEren-Aksu <turguterenaksu@outlook.com>
2024-10-09 00:01:24 +0300

    getPortfolio

--- SwiftUICrypto/Services/PortfolioDataService.swift
+++ SwiftUICrypto/Services/PortfolioDataService.swift
@@ -11,12 +11,23 @@ import CoreData
 class PortfolioDataService{
     private let container: NSPersistentContainer
     private let containerName: String = "PortfolioContainer"
+    private let entityName: String = "PortfolioEntity"
+    
+    @Published var savedEntities:[PortfolioEntity] = []
     init(){
         container = NSPersistentContainer(name: containerName)
         container.loadPersistentStores { (_, error) in
             if let error = error{
                 print(error.localizedDescription)
             }
+        }
+    }
+    private func getPortfolio(){
+        let request = NSFetchRequest<PortfolioEntity>(entityName: entityName)
+        do{
+            savedEntities = try container.viewContext.fetch(request)
+        }catch let error{
+            print("error: \(error)")
         }
     }
 }
